33e176628133140ea00a40b5cf3c4e98b4383843
yhchen24 <yhc724@uw.edu>
2020-03-18 03:44:09 -0700

    edit

--- missingdensityprotein/neural_networks/nnm_coordinate.py
+++ missingdensityprotein/neural_networks/nnm_coordinate.py
@@ -1,11 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import math
-# from mpl_toolkits import mplot3d
 from sklearn.model_selection import train_test_split
 
 seed = 23
@@ -82,55 +78,3 @@ def get_mse(filename):
     history = run_model(filename)[0]
     print("final MSE for train is %.2f and for validation is %.2f" %
           (history.history['loss'][-1], history.history['val_loss'][-1]))
-
-
-def plot_validation(filename):
-    df2 = validation_coord(filename)
-    [X_pred, Y_pred, Z_pred, X_real, Y_real, Z_real] = [df2['X pred'],
-                                                        df2['Y pred'],
-                                                        df2['Z pred'],
-                                                        df2['X test'],
-                                                        df2['Y test'],
-                                                        df2['Z test']]
-    min_val = min(min(X_pred), min(Y_pred), min(Z_pred), min(X_real),
-                  min(Y_real), min(Z_real))
-    max_val = max(max(X_pred), max(Y_pred), max(Z_pred), max(X_real),
-                  max(Y_real), max(Z_real))
-    plt.figure(figsize=(6, 6))
-    plt.subplot()
-    plt.plot(np.linspace(math.floor(min_val) - 5,
-                         math.ceil(max_val) + 5,
-                         1001),
-             np.linspace(math.floor(min_val) - 5,
-                         math.ceil(max_val) + 5,
-                         1001),
-             color='black', label='X = Y')
-    plt.scatter(X_pred, X_real, label='X coordinate')
-    plt.scatter(Y_pred, Y_real, label='Y coordinate')
-    plt.scatter(Z_pred, Z_real, label='Z coordinate')
-    plt.xlabel('$Predicted$ $Coordinate$', fontsize='16')
-    plt.ylabel('$Real$ $Coordinate$', fontsize='16')
-    plt.xlim([math.floor(min_val) - 5, math.ceil(max_val) + 5])
-    plt.ylim([math.floor(min_val) - 5, math.ceil(max_val) + 5])
-    plt.legend()
-    plt.grid()
-
-
-def plot_validation_atoms(filename):
-    df2 = validation_coord(filename)
-    [X_pred, Y_pred, Z_pred, X_real, Y_real, Z_real] = [df2['X pred'],
-                                                        df2['Y pred'],
-                                                        df2['Z pred'],
-                                                        df2['X test'],
-                                                        df2['Y test'],
-                                                        df2['Z test']]
-    plt.figure(figsize=(10, 10))
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(X_pred, Y_pred, Z_pred, label='predicted atoms')
-    ax.scatter3D(X_real, Y_real, Z_real, label='real atoms')
-    ax.set_title('Atomes of Protein', fontsize='20')
-    ax.set_xlabel('$X$', fontsize='14')
-    ax.set_ylabel('$Y$', fontsize='14')
-    ax.set_zlabel('$Z$', fontsize='14')
-    ax.legend(fontsize='12')
-    ax.view_init(elev=45, azim=60)
